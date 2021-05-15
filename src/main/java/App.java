80cc3870446b83b109ba620f7a22c146ec9ae10f
Hummock <shatkovsky-toros@yandex.ru>
2021-05-16 01:49:47 +0300

    2.1.2 - Cat & Hello, World

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean1.getMessage() == bean2.getMessage());
+        System.out.println(beanCat1.getMessage() != beanCat2.getMessage());
+
+    }
+}
